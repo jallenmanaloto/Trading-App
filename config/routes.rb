3c1e8048f8df9c21c36cfa21fc5a074df6f5465d
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-23 14:38:17 +0800

    Set route for search method on Market

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'users#index'
   get 'index', to: 'markets#index'
+  get 'search', to: 'markets#search'
   devise_for :users, controllers: { registrations: 'users/registrations' }, views: { registrations: 'users/new' }
 
   namespace :api do
