78d952ef47b1e0e4120a3265fb40e3aea879df33
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-23 14:59:01 +0800

    Fixed issue not finding ID of chosen stock to show

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,12 +1,13 @@
 class MarketsController < ApplicationController
 
     def index
-        market = Market.first # change to find id of chosen stock
+        market = Market.find(params[:id]) # change to find id of chosen stock
         render json: {
             current_price: market.latest_price,
             company: market.company_info,
             logo: market.logo,
-            news: market.news_company
+            news: market.news_company,
+            charts: market.chart
         }
     end
 
