0cc9a398a1f932a1b829d6b08f4bb8fdf51073a9
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-24 15:46:10 +0800

    Include Stock model resources

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
-        resources :traders
+        resources :traders do
+          resources :stocks
+        end
         resources :admins
       end
     end
