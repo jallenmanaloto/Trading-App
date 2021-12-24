7c0be1137fcb7044eca974c79efee9fa6820331e
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-24 15:45:38 +0800

    Redefine methods

--- app/models/trader.rb
+++ app/models/trader.rb
@@ -2,4 +2,17 @@ class Trader < ApplicationRecord
     has_many :stocks
     belongs_to :user
     
+    def deposit_money 
+        # cash in money which will be used to buy stocks
+        # default for new trader = 0
+        # current money + new deposits
+    end
+
+    def buy_stock
+        # deduct money from current cash
+    end
+
+    def sell_stock
+        # adds money to current cash
+    end
 end
