0937ae745e2af3e1b29e79d29c4194b5582bffb4
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-24 15:45:05 +0800

    Add IEX configuration and method to fetch latest price and change percent

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,19 @@
 class Stock < ApplicationRecord
     belongs_to :trader
+
+    @@client = IEX::Api::Client.new(
+            publishable_token: 'pk_dea2655fa642467f92990a1f0b4885d9',
+            secret_token: 'sk_0810686b64154091a0432199577c12ae',
+            endpoint: 'https://cloud.iexapis.com/v1'
+        )
+
+    def latest_price
+        quote = @@client.quote(self.symbol)
+        quote.latest_price
+    end
+
+    def change_percent
+        quote = @@client.quote(self.symbol)
+        quote.change_percent
+    end
 end
