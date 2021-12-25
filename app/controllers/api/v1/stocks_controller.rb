44dbc5200b2d74f508b9f800c869054607b1ffaa
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-25 21:59:09 +0800

    Fixed error message on json render for stocks

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -5,26 +5,36 @@ module Api
             respond_to :json
 
             def index
-                stocks = Stock.all
-                render json: { stocks: stocks }
+                trader = Trader.find(params[:trader_id])
+                stocks = trader.stocks 
+                trader_stock = stocks.group_by { |stock| stock[:symbol] }
+                .values
+                .map do |_, stock|
+                    stock.reduce do |a, b|
+                        a.merge(b) {|k, ov, nv| k == :symbol ? ov : ov + nv}
+                    end
+                end
+                render json: { stocks: trader_stock }
+
             end
 
             def new
             end
 
             def create
-                stock = Stock.create(stock_params.merge(latest_price: stock.latest_price, change_percent: stock.change_percent))
+                stock = Stock.create(stock_params)
                 if stock.save
+                    stock.update(latest_price: stock.latest_price.to_f.round(2), change_percent: stock.change_percent.to_f.round(5))
                     render json: { stock: stock }
                 else
-                    format.json { render json: user.errors, status: :unprocessable_entity }
+                    format.json { render json: stock.errors, status: :unprocessable_entity }
                 end
             end
 
             private
 
             def stock_params
-                params.require(:stock).permit(:name, :symbol)
+                params.permit(:name, :symbol, :trader_id, :quantity)
             end
         end
     end
