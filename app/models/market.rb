bf5db435b5bfe7de947778afd41a54ee527c3432
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-26 14:43:15 +0800

    Reconfigure IEX tokens

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,8 +1,8 @@
 class Market < ApplicationRecord
 
     @@client = IEX::Api::Client.new(
-            publishable_token: 'pk_dea2655fa642467f92990a1f0b4885d9',
-            secret_token: 'sk_0810686b64154091a0432199577c12ae',
+            publishable_token: 'pk_a020ad363b0b447185bf2db1cfd11154',
+            secret_token: 'sk_4e277e5f5d884ae987d56fca67577a68',
             endpoint: 'https://cloud.iexapis.com/v1'
         )
 
