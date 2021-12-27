abd016d5d647062f67fd51e70e7804c8df16edbf
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-27 16:12:36 +0800

    Added routes for history

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,17 @@ Rails.application.routes.draw do
   get 'index', to: 'markets#index'
   get 'search', to: 'markets#search'
   get 'api/v1/users/:user_id/traders/:trader_id/stock_quantity', to: 'api/v1/stocks#stock_quantity'
+  # patch 'api/v1/users/:user_id/traders/:trader_id/sell_stock', to: 'api/v1/traders#sell_stock'
   devise_for :users, controllers: { registrations: 'users/registrations' }, views: { registrations: 'users/new' }
 
   namespace :api do
     namespace :v1 do
+      resource :markets
       resources :users do
         resources :traders do
           patch :buy_stock, on: :member
+          patch :sell_stock, on: :member
+          resources :histories
           resources :stocks
         end
         resources :admins
