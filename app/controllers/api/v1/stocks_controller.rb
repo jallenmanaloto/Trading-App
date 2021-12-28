badddbb456a470979e935e13be46ef61e96a99c3
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-28 16:42:54 +0800

    removed methods on controlelr

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -11,46 +11,6 @@ module Api
                 render json: { stocks: stocks}
             end
 
-            # To be deleted and migrate creation of new/update of stocks to buy_stock method
-            def stock_quantity
-                trader = Trader.find(params[:trader_id])
-                stocks = trader.stocks
-                @trader_sum_stock = stocks.group_by { |stock| stock[:symbol] }
-                .values
-                .map do |a| 
-                    { stock: a.first.name , total_shares: a.map(&:quantity).inject(0, &:+), all: a }
-                end
-                               
-                render json: { stocks: @trader_sum_stock}
-            end
-
-            def buy_stock
-                # use find_or_create_by method to create or update a chosen stock
-            end
-
-
-            def sell_stock
-                trader = Trader.last
-                render json: { sell_stock: trader }
-            end
-
-            def create
-                stock = Stock.create(stock_params)
-                amount = params[:amount_bought].to_f
-                logo = params[:symbol]
-                if stock.save
-                    stock.update(
-                        latest_price: stock.latest_price.to_f.round(2), 
-                        change_percent: stock.change_percent.to_f.round(5), 
-                        quantity: stock.calculate_quantity(amount), 
-                        company_logo: stock.company_logo(logo)
-                    )
-                    render json: { stock: stock }
-                else
-                   render json: stock.errors
-                end
-            end
-
             private
 
             def stock_params
