65a59f8e346d2eb82d5ebeca8a7fe8f59a110ec1
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-29 14:34:30 +0800

    Removed creating of transaction history

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -1,27 +1,12 @@
 module Api
     module V1
         class HistoriesController < ApplicationController
+            skip_before_action :verify_authenticity_token
             def index
                 trader = Trader.find(params[:id])
                 history = trader.histories.all
 
                 render json: { history: history }
-            end
-
-            def create
-                trader = Trader.find(params[:id])
-                history = trader.histories.build(history_params)
-                if history.save
-                    render json: { history: history }
-                else
-                    render json: { error: history.errors }
-                end
-            end
-
-            private
-
-            def history_params
-                params.require(:history).permit(:transaction, :stock_name, :symbol, :quantity, :price)
             end
         end
     end
