203f49e11d11c7c5cccb41d3a5a7718c3b4b2044
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-29 21:44:33 +0800

    Add validations for attributes

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,3 +1,5 @@
 class History < ApplicationRecord
+    validates_presence_of :transaction_name, :stock_name, :symbol, :quantity, :price
+    validates :transaction_name, inclusion: { in: %w(Buy Sell) }
     belongs_to :trader
 end
