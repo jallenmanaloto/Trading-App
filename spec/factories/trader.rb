76d9374e9161a60e473b8925b1f82c846f510567
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-29 21:45:46 +0800

    Created FactoryBot for trader instance

---
+++ spec/factories/trader.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+    factory (:trader) do
+        association :user
+        
+        name { "test01" }
+        status { false }
+        total_cash { 0.00 }
+        total_stocks { 0.00 }
+        profit { 0.00 }
+        email { "test01@email.com" }
+        equity { 0.00 }
+    end
+end
