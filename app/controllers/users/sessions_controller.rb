75f1a430733575a6c137f6bfb679dc6fc8b633e8
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-30 17:28:42 +0800

    Configure session create for root path

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,17 +1,23 @@
 # frozen_string_literal: true
 
-class User::SessionsController < Devise::SessionsController
+class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  skip_before_action :verify_authenticity_token
+  # respond_to :json
   # GET /resource/sign_in
   # def new
   #   super
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    # super
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: root_path
+  end
 
   # DELETE /resource/sign_out
   # def destroy
