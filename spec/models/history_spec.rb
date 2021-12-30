cd9dd3d8a1e61aa5bd776512c02d3c87bdae269f
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-30 17:34:47 +0800

    Created RSpec test for History model

--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -1,9 +1,127 @@
 require 'rails_helper'
 
 describe History, type: :model do
-    context 'Validations' do
-        it '' do 
-            
+    context 'Validations with empty attributes' do
+        it 'should return error if transaction_name is nil' do 
+            history = History.create(
+                :transaction_name => nil,
+                :stock_name => "Tesla",
+                :symbol => "TSLA",
+                :quantity => "10",
+                :price => "100"
+            )
+
+        expect(history).to_not be_valid
+        expect(history.errors).to be_present
+        end
+
+        it 'should return error if stock_name is nil' do 
+            history = History.create(
+                :transaction_name => "Buy",
+                :stock_name => nil,
+                :symbol => "TSLA",
+                :quantity => "10",
+                :price => "100"
+            )
+
+        expect(history).to_not be_valid
+        expect(history.errors).to be_present
+        end
+
+        it 'should return error if symbol is nil' do 
+            history = History.create(
+                :transaction_name => "Buy",
+                :stock_name => "Tesla",
+                :symbol => nil,
+                :quantity => "10",
+                :price => "100"
+            )
+
+        expect(history).to_not be_valid
+        expect(history.errors).to be_present
+        end
+
+        it 'should return error if quantity is nil' do 
+            history = History.create(
+                :transaction_name => "Buy",
+                :stock_name => "Tesla",
+                :symbol => "TSLA",
+                :quantity => nil,
+                :price => "100"
+            )
+
+        expect(history).to_not be_valid
+        expect(history.errors).to be_present
+        end
+
+        it 'should return error if price is nil' do 
+            history = History.create(
+                :transaction_name => "Buy",
+                :stock_name => "Tesla",
+                :symbol => "TSLA",
+                :quantity => "10",
+                :price => nil
+            )
+
+        expect(history).to_not be_valid
+        expect(history.errors).to be_present
+        end
+    end
+
+    context 'Validations with correct parameters' do
+        it 'should be valid and no error return if all attributes are not empty' do
+            history = FactoryBot.create(:history)
+
+            expect(history).to be_valid
+        end
+    end
+
+    context 'Valid table transaction_name' do
+        it 'will not return error if equal to Buy' do
+            history = FactoryBot.create(:history,
+                :transaction_name => "Buy",
+                :stock_name => "Tesla",
+                :symbol => "TSLA",
+                :quantity => "10",
+                :price => "100"
+            )
+
+            expect(history).to be_valid
+            expect(history.transaction_name).to eq("Buy")
+        end
+
+        it 'will not return error if equal to Sell' do
+            history = FactoryBot.create(:history,
+                :transaction_name => "Sell",
+                :stock_name => "Tesla",
+                :symbol => "TSLA",
+                :quantity => "10",
+                :price => "100"
+            )
+
+            expect(history).to be_valid
+            expect(history.transaction_name).to eq("Sell")
+        end
+
+        it 'will return an error if equals to other value' do
+            history = FactoryBot.build(:history,
+                :transaction_name => "Diff",
+                :stock_name => "Tesla",
+                :symbol => "TSLA",
+                :quantity => "10",
+                :price => "100"
+            )
+
+            expect(history).to_not be_valid
+            expect(history.errors).to be_present
+        end
+    end
+
+    context 'Association with Trader' do
+        it 'should be defined as belongs_to trader' do
+            history = History.reflect_on_association(:trader)
+
+            expect(history.macro).to eq(:belongs_to)
         end
     end
 end
