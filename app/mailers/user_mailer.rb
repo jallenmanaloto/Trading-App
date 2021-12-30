1303964a24c26a475c7d3f4edb27c4aecbf5c3ad
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-30 17:29:03 +0800

    Changed subject for email sent

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,12 @@ class UserMailer < ApplicationMailer
     default template_path: 'user_mailer'
 
     def approve_account(user)
-        email = user.email
+        email = user.email.to_s
         token = user.confirmation_token
         mail(
             from: "support@project.com",
             to: email, 
-            subject: "Approved account",
+            subject: "Pending Account",
         )
     end
 end
