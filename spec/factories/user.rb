3c1b94da22791a8f91ae1c165087b3a3a9fbf014
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-30 17:33:35 +0800

    Edited details in factorybot

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
     factory(:user) do
-        name { "samplename" }
-        email { "test2@test.com" }
+        name { "test01" }
+        email { "test01@email.com" }
         password { "password1234" }
+        id { 5 }
     end
 end
