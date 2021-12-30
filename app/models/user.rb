4f3c523334a1b1fe5cc49fc62086e6e285c0c574
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-30 17:30:33 +0800

    Add validation for name, email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,15 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable, :confirmable
     #validates_presence_of :name, presence_of: :email, presence_of: :password
-    validates :name, length: { minimum: 5, maximum: 20 },
-        uniqueness: true
+    validates :name, length: { minimum: 5, maximum: 20 }
     validates_presence_of :name, :email, :password
         
 
 
     has_one :admin
     has_one :trader
-    before_create :skipping_confirm
-    after_create :send_pending
+    # before_create :skipping_confirm
+    # after_create :send_pending
 
     def skipping_confirm
         self.skip_confirmation!
