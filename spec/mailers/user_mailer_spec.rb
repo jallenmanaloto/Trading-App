945b9d7c544d9f5bb4fbff174ca272811bdda600
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-30 17:34:18 +0800

    Created RSpec test for UserMailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = FactoryBot.build(:user)
+  let(:mail) { UserMailer.approve_account(user) }
+
+  describe 'UserMailer' do
+    context 'sends email to user' do
+      it 'where email is equal to user email' do
+        expect(mail.to).to eq([user.email])
+      end
+
+      it 'where sender is equal to support@project.com' do
+        expect(mail.from).to eq(["support@project.com"])
+      end
+
+      it 'where subject is equal to Pending Account' do
+        expect(mail.subject).to eq("Pending Account")
+      end
+    end
+  end
 end
