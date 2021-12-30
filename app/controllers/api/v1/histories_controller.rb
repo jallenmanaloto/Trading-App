67ae742ce85f8e76b37ce1b9be9dea35f0d29b71
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-30 17:26:48 +0800

    Fixed parameters for searching user

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -3,10 +3,11 @@ module Api
         class HistoriesController < ApplicationController
             skip_before_action :verify_authenticity_token
             def index
-                trader = Trader.find(params[:id])
+                user = User.find(params[:user_id])
+                trader = user.trader
                 history = trader.histories.all
 
-                render json: { history: history }
+                render json: { history: history }, status: :ok
             end
         end
     end
