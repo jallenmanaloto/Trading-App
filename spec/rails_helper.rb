8c1ac8c48eec4f521d69c4e3941fbed43b5a0838
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-30 17:35:31 +0800

    Add FactoryBot Syntax Methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::IntegrationHelpers
+  config.include Devise::TestHelpers, :type => :controller
+  config.include FactoryBot::Syntax::Methods
+  # config.use_transactional_fixtures = true
+
+  # FactoryBot.register_strategy(:attr_strat, AttributeStrategy)
 end
